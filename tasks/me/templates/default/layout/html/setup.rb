fbbc8a0ab4aaaffa80f96112d185c382dd738bf5
Marian <marian@bugslayer.de>
2014-03-09 20:27:03 +0100

    fehlbaum.

--- tasks/me/templates/default/layout/html/setup.rb
+++
@@ -1,9 +0,0 @@
-#def init
-#    super
-#    sections.push :todolist
-#end
-
-def menu_lists
-  super + [ { type: 'todolist', title: 'Todo List', search_title: 'Undone List' } ]
-  # note: "type:" refers to the generator method (generate_x_list)
-end
