e6b71289286d503fbba3ff10911be07cbd27d6cc
Marian Eichholz <marian.eichholz@freenet.ag>
2014-03-11 16:01:48 +0100

    fix des Syncers.

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,12 +1,11 @@
 # vim: ft=sh
-SUBDIRS="tasks vim bin yard"
 MEPATH=projects/devsupport
 PMPATH=pm-git/software/devsupport
 
-for i in $SUBDIRS ; do
+PAIRS="projects/dotvim:pm-git/config/puppet/modules/editors/files/dotvim"
+for i in tasks vim bin yard ; do
 	PAIRS="$PAIRS $MEPATH/$i:$PMPATH/$i"
 done
-cd $HOME || exit 3
 
 DRY_MODE="--dry-run"
 if [ "$1" = "--force" ]; then
@@ -19,12 +18,13 @@ MODE="$1"
 syncThem()
 {
   local their_dir my_dir pair modes
-  their_dir=`echo $pair | cut -d: -f$1`
-  my_dir=`echo $pair | cut -d: -f$2`
+
+  cd $HOME || exit 3
 
   for pair in $PAIRS ; do
-	  my_dir=`echo $pair | cut -d: -f2`
-	  echo "comparing <$my_dir> to <$their_dir>"
+	  their_dir=`echo $pair | cut -d: -f$1`
+	  my_dir=`echo $pair | cut -d: -f$2`
+	  echo ">>> rsyncing <$my_dir> to <$their_dir>"
 	  modes="-rtu --verbose --cvs-exclude"
 	  modes="$modes $DRY_MODE"
 	  rsync $modes "$my_dir"/ "$their_dir" # slash on source is important
@@ -33,6 +33,7 @@ syncThem()
 
 checkSanity()
 {
+    cd $HOME || exit 3
 	test -r $PMPATH || exit 1
 
 	# just for sanity: update it
@@ -44,22 +45,23 @@ checkSanity()
 
 diffThem()
 {
+    cd $HOME || exit 3
 	for pair in $PAIRS ; do
-		my_dir=`echo $pair | cut -d: -f2`
-		their_dir=`echo $pair | cut -d: -f1`
+		my_dir=`echo $pair | cut -d: -f$2`
+		their_dir=`echo $pair | cut -d: -f$1`
 		diff -r $my_dir $their_dir
 	done
 }
 
 case "$MODE" in
-	diff) diffThem ;;
+	diff) diffThem 1 2 ;;
 	to-fn)
 		checkSanity
-        syncThem 2 1 # to FN
+        syncThem 2 1
 		;;
 	from-fn)
 		checkSanity
-		syncThem 1 2 # from FN
+		syncThem 1 2
 		;;
 	*) echo "USAGE: sh sync.sh [--force] diff|to-fn|from-fn"
 	   ;;
