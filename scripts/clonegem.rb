c8eeb3be741bdda862c3d5b69d1d04bc53d19684
Marian Eichholz <marian.eichholz@freenet.ag>
2015-06-19 17:06:51 +0200

    fixed: internal structure.

--- scripts/clonegem.rb
+++ scripts/clonegem.rb
@@ -5,43 +5,66 @@ def panic(reason)
   exit 1
 end
 
-def guess_project(directory)
-  File.basename(directory)
+def verbalize(level, s)
+  puts s
+end
+
+# Project collects characteristics of the source and destination projects
+class Project
+
+  attr_reader :path, :dirname, :modulename, :style
+
+  def initialize(opt={})
+    @path = opt.fetch(:path, nil)
+    @dirname = opt.fetch(:dirname, guess_dirname(@path))
+    @modulename = opt.fetch(:modulename, guess_modulename(@path))
+    @style = :hoe
+  end
+
+  def guess_dirname(dir)
+    File.basename(dir)
+  end
+
+  def guess_modulename(dir)
+    File.basename(dir).capitalize
+  end
+
 end
 
 class GemCloner
 
-  def initialize(opt={})
-    @from_dir = opt.fetch(:from_dir, nil)
-    @from_project = guess_project(@from_dir)
-    @project = guess_project(Dir.pwd)
+  def initialize(src, dest)
+    @src = src
+    @dest = dest
   end
 
   def check
-    puts "using: #{@from_dir} as template"
-    panic('need from_dir') unless Dir.exist?(@from_dir)
-    # @todo check for pristinity
+    verbalize :info, "using: #{@src.path} as template"
+    panic("#{src.dir} not existing") unless Dir.exist?(@src.path)
     files = Dir['.git', '*']
     return true if files.empty?
     false
   end
 
   def copy
-    puts "copying over: #{@from_dir}"
+    srcdir = @src.path
+    verbalize :info, "copying over: #{srcdir}"
     files = Array.new
-    Dir.chdir @from_dir do
+    Dir.chdir srcdir do
       files = Dir['bin/*', 'lib/**/*.rb', '*.md', 'spec/**/*.rb', 'features/**/*.rb']
-      [ 'Rakefile', 'local.vim', '.projections.json'].each do |file|
+      [ 'Manifest.txt', 'Rakefile', 'local.vim', '.projections.json'].each do |file|
         files << file
       end
     end
+    @allfiles = Array.new
     files.each do |file|
-      fromfile = File.join(@from_dir, file)
-      tofile = file.gsub(@from_project, @project)
+      fromfile = File.join(srcdir, file)
+      tofile = file.gsub(@src.dirname, @dest.dirname)
       path = File.dirname(tofile)
-      puts "getting: #{tofile}"
+      verbalize :info, "getting: #{tofile}"
       FileUtils.mkdir_p path unless Dir.exist?(path)
       FileUtils.cp fromfile, tofile
+      @allfiles << tofile
     end
   end
 
@@ -50,16 +73,26 @@ class GemCloner
     # TODO: find all files and run them through a filter
     # TODO: patch all path references (oldproject, newproject)
     # TODO: patch all NameSpace references
+    @allfiles.each do |file|
+      patch_file file
+    end
   end
+
+  def patch_file(file)
+    verbalize :info, "patching #{file}"
+  end
+
 
 end
 
-from_dir = File.join(ENV['HOME'],'projects/gems/reco')
+src = Project.new(path: File.join(ENV['HOME'], 'projects/gems/reco'),
+                 )
 
-cloner = GemCloner.new(from_dir: from_dir,
-                      )
+dest = Project.new(path: Dir.pwd)
 
-cloner.check || panic('project not pristine enough')
+cloner = GemCloner.new(src, dest)
+
+cloner.check || panic('project not pristine')
 cloner.copy
 cloner.patch
 
