832f12d0195f48544d8786b15c6921300e86429f
Marian Eichholz <marian.eichholz@freenet.ag>
2015-06-22 12:49:24 +0200

    added: infrastructure git creation mode.

--- scripts/clonegem.rb
+++ scripts/clonegem.rb
@@ -1,12 +1,19 @@
 require 'fileutils'
 
+class Options
+  @verbose = true
+  class << self
+    attr_accessor :verbose
+  end
+end
+
 def panic(reason)
   STDERR.puts "FATAL: #{reason}"
   exit 1
 end
 
 def verbalize(level, s)
-  puts s
+  puts "#{level}: #{s}" if Options::verbose
 end
 
 # Project collects characteristics of the source and destination projects
@@ -21,6 +28,11 @@ class Project
     @style = :hoe
   end
 
+  def devsupport_url
+    # @todo may be inferred from source and cloned later
+    'ssh://giles.bugslayer.de/srv/git/projects/devsupport'
+  end
+
   def guess_dirname(dir)
     File.basename(dir)
   end
@@ -33,9 +45,29 @@ end
 
 class GemCloner
 
-  def initialize(src, dest)
+  def initialize(src, dest, opt={})
     @src = src
     @dest = dest
+    @opt_create = opt.fetch(:create, false)
+  end
+
+  def inside_destination
+    dir = @dest.path
+    panic "cannot chdir to `#{dir}'" unless Dir.exist?(dir)
+    Dir.chdir dir do
+      yield
+    end
+  end
+
+  def create!
+    destdir = @dest.path
+    verbalize :info, "check and create `#{destdir}'"
+    return false if Dir.exist? destdir
+    system("git init #{@dest.dirname}")
+    inside_destination do
+      system("git submodule add #{@src.devsupport_url}")
+    end
+    true
   end
 
   def check
@@ -46,7 +78,7 @@ class GemCloner
     false
   end
 
-  def copy
+  def copy!
     srcdir = @src.path
     verbalize :info, "copying over: #{srcdir}"
     files = Array.new
@@ -68,7 +100,7 @@ class GemCloner
     end
   end
 
-  def patch
+  def patch!
     # TODO: determine old and new NameSpaces :-)
     # TODO: find all files and run them through a filter
     # TODO: patch all path references (oldproject, newproject)
@@ -82,19 +114,37 @@ class GemCloner
     verbalize :info, "patching #{file}"
   end
 
+  def run!
+    if @opt_create
+      create! || panic('cannot create empty project frame')
+    else
+      inside_destination do
+        check || panic('project not pristine')
+      end
+    end
+    inside_destination do
+      copy!
+      patch!
+    end
+  end
 
 end
 
-src = Project.new(path: File.join(ENV['HOME'], 'projects/gems/reco'),
+# @todo: take from ARGV
+@newpro_dir = 'guiframe'
+@newpro_module = 'GuiFrame'
+
+src = Project.new(path: File.join(ENV['HOME'], 'projects', 'gems', 'reco'),
                  )
+dest = Project.new(path: File.join(Dir.pwd, @newpro_dir),
+                   module_name: @newpro_module,
+                  )
 
-dest = Project.new(path: Dir.pwd)
+cloner = GemCloner.new(src, dest,
+                      create: true,
+                      )
+cloner.run!
 
-cloner = GemCloner.new(src, dest)
-
-cloner.check || panic('project not pristine')
-cloner.copy
-cloner.patch
 
 
 
